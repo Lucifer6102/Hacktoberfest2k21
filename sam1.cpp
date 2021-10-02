af63d04ecfc132ff3387029646a8b1c4542b737e
Rishi Gupta <56929890+Rishi6223@users.noreply.github.com>
2021-10-02 23:39:36 +0530

    Added CPP file

---
+++ sam1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define N 1000
+#define M 1000000007
+#define f(i, a, b) for (ll i = (ll)a; i <= (ll)b; i++)
+#define rf(i, a, b) for (ll i = (ll)a; i >= (ll)b; i--)
+#define po pop_back
+#define pb push_back
+#define lb lower_bound
+#define fi first
+#define se second
+#define mp make_pair
+#define bs binary_search
+#define debug(x) cout << #x << " = " << x << endl
+#define ub upper_bound
+#define ibs ios_base::sync_with_stdio(false)
+#define cti cin.tie(0)
+#define all(x) x.begin(), x.end()
+#define PI 3.14159265
+#define cot cout.tie(0)
+using namespace std;
+/**
+  * @author :: Rishabh
+  *
+  */
+int main()
+{
+    char c,a,b;
+    c='f';
+    a='s';
+    b='x';
+    int sum=c+a+b;
+    cout<<sum;
+}
