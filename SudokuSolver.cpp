405bf1d5d8619be0b6799362066c21c74935ddcd
Ankit Lenka <ankitlenka26@gmail.com>
2021-10-05 22:28:03 +0530

    Sudoku solver algorithm

---
+++ SudokuSolver.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// to check if the the position searched is valid or not 
+bool isValid(vector<vector<char>> board,int po,int i,int j){
+    char c='0'+po;
+    for(int m=0;m<board.size();m++){
+        if(board[i][m]==(c))
+            return false;
+    }
+    for(int m=0;m<board.size();m++){
+        if(board[m][j]==(c))
+            return false;
+    }
+    int sub_i=3*(i/3);
+    int sub_j=3*(j/3);
+    for(int m=0;m<3;m++){
+        for(int n=0;n<3;n++){
+            if(board[sub_i+m][sub_j+n]==(c))
+                return false;
+        }
+    }
+    return true;
+}
+
+// solving the sudoku solver using backtracking
+bool solve (vector<vector<char>> &board,int i,int j){
+    int ni,nj;
+    // end of board 
+    if(i==board.size()){
+
+        return true;
+    }
+    // end of row , switch to new row 
+    if(j==(board[0].size()-1)){
+        ni=i+1;nj=0;
+    } // if the row has not ended then simply just increment the value of the column 
+    else{
+        nj=j+1;ni=i;
+    }
+
+    // backtracking
+    if(board[i][j]=='0'){
+        for(int po=1;po<=9;po++){
+            if(isValid(board,po,i,j)){
+                     char c='0'+po;
+                board[i][j]=c;
+                if(solve(board,ni,nj))
+                    return true;
+                board[i][j]='0';
+            }
+        }
+    }
+    else
+    {
+        if(solve(board,ni,nj))
+                    return true;
+    }
+    return false;
+}
+
+int main() {
+    // enter the initial configuration of the board 
+       vector<vector<char>>board(9,vector<char>(9,'0'));
+       cout<<"Enter the 9*9 sudoku :\n\n";
+      for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cin>>board[i][j];
+        }
+    }
+    int ni=0,nj=0;//ni=next i,nj=next j
+    solve(board,ni,nj);
+
+    cout<<"\n\n\nThe solved sudoku is : \n\n\n";
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    }
